3e0d7046465c294a5143675837d274e185fd168e
Noah <barnesnoah@icloud.com>
2019-10-09 12:57:29 -0400

    Finished signup and login features, started working on importing from csv file

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Project 1
 
 Web Programming with Python and JavaScript
+
+
+
+File Directories:
+
+All html files can be found under the "templates" folder.
+
+All files related to css can be found under the "css" folder.
+
+All files related to SQL, databases, and table creation can be found under the "sql" folder.
+
+The provided "books.csv" file as well as the required "import.py" file can be found under the "importing" folder.
+
+
+
+SOURCES:
+
+Hashcode encrypting and verification functions: https://www.vitoshacademy.com/hashing-passwords-in-python/
+                                                https://pythonprogramming.net/password-hashing-flask-tutorial/
+If-statement to check if a sequence is empty: https://stackoverflow.com/questions/53513/how-do-i-check-if-a-list-is-empty
--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask, session
+from passlib.hash import sha256_crypt
+
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -17,10 +19,72 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
+# engine = create_engine(os.getenv("postgres://plrueqrfqoawfm:dba8ea014066e4f18d3b8813c1f1e103b1a104d983303ffe92ad2c1ecca3ddab@ec2-54-235-246-201.compute-1.amazonaws.com:5432/d7kuluc1gil68"))
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", unsuccessful='False')
+
+@app.route("/registered", methods=["POST"])
+def registered():
+    un = request.form.get("username")
+    pw = request.form.get("password")
+
+
+    db.execute("BEGIN")
+
+    prevUsers = db.execute("SELECT * FROM accounts WHERE username = :username", {"username": un}).fetchall()
+
+    if prevUsers:
+        db.execute("COMMIT")
+        return render_template("signup.html", unsuccessful=True)
+    else:
+        db.execute("INSERT INTO accounts (username, password) VALUES (:username, :password)",
+                     {"username": un, "password": hash_password(password=pw)})
+        db.execute("COMMIT")
+        return render_template("registered.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", unsuccessful=False)
+
+@app.route("/logedin", methods=["POST"])
+def logedin():
+    un = request.form.get("username")
+    pw = request.form.get("password")
+
+    db.begin()
+
+    storedPW = db.execute("SELECT password FROM accounts WHERE username = :username", {"username": un}).fetchone()
+
+    db.commit()
+
+    if(verify_password(storedPW[0], pw)):
+        # TODO: set some sort of "loged in" variable to True
+        return render_template("logedin.html")
+    else:
+        return render_template("login.html", unsuccessful=True)
+
+
+
+def hash_password(password):
+
+    return sha256_crypt.encrypt(password)
+
+
+
+
+
+
+def verify_password(stored_password, provided_password):
+
+
+    return sha256_crypt.verify(provided_password, stored_password)
