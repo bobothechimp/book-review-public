b4821cab92c178066ab043ba1ba467e71f81cbb4
Noah <barnesnoah@icloud.com>
2019-10-10 21:47:15 -0400

    finished project1 requirements (yay)

--- README.md
+++ README.md
@@ -2,17 +2,59 @@
 
 Web Programming with Python and JavaScript
 
+My project is (of course) a book reviewing website called "Book Talks". People can create accounts, log into those accounts, search through the catalog of 5000 books, and leave reviews for those books. They can even use the API by following the "/api/<isbn>" path in the URL.
 
 
-File Directories:
 
-All html files can be found under the "templates" folder.
+FILE DIRECTORIES:
+
+All html files can be found under the "templates" folder. This includes:
+  - layout.html, the layout for all pages
+  - index.html, the home page
+  - signup.html, the sign up page
+  - registered.html, the page telling users they've signed up
+  - login.html, the log in page
+  - loggedin.html, the page telling users they've logged in
+  - logout.html, the page telling users they've logged out
+  - catalog.html, the catalog page
+  - catalogBook.html, the page displaying information about a certain book
+
 
 All files related to css can be found under the "css" folder.
 
 All files related to SQL, databases, and table creation can be found under the "sql" folder.
+  - accounts.sql, the information of all registered accounts
+  - books.sql, the information of all 5000 books
+  - reviews.sql, the information of all reviews of all books
 
 The provided "books.csv" file as well as the required "import.py" file can be found under the "importing" folder.
+
+
+DATABASE TABLES:
+
+"accounts"
+  "username", a text column to store an account's username
+  "password", a text column to store an account's (hashed) password
+  "user_id", a serial key to represent the id of a certain account
+
+"books"
+  "title", a text column representing the title of the book
+  "author", a text column representing the author of the book
+  "year", an integer column representing the year the book was published
+  "isbn", a text column representing the book's ISBN
+
+"reviews"
+  "poster", a text column representing the username of the person who posted the review
+  "review_text", a text column representing the actual review itself
+  "isbn", a text column representing the isbn of the book the review is for
+
+
+
+EXTRA NOTES:
+
+For some reason the font size and padding of some classes (.toplinks, .desc, etc.) would not change when a font size or padding size was specified in the stylesheet, so I had to include the styling inside the head of "layout.html"
+
+I'm sorry if some parts of the website don't look great visually; I was in a bit of a rush to finish the project after I had lots of trouble with the setup and installing everything.
 
 
 
@@ -21,3 +63,5 @@ SOURCES:
 Hashcode encrypting and verification functions: https://www.vitoshacademy.com/hashing-passwords-in-python/
                                                 https://pythonprogramming.net/password-hashing-flask-tutorial/
 If-statement to check if a sequence is empty: https://stackoverflow.com/questions/53513/how-do-i-check-if-a-list-is-empty
+Documentation for bootstrap list group component: https://getbootstrap.com/docs/4.3/components/list-group/
+Documentation for bootstrap form component: https://getbootstrap.com/docs/4.3/components/forms/
--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 import os
+import requests
 
 from passlib.hash import sha256_crypt
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, jsonify, json
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -25,14 +26,22 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+
+#Home page
 @app.route("/")
 def index():
-    return render_template("index.html")
 
+    if session.get("currentUserID") is None:
+        return render_template("index.html", logedin='False')
+    else:
+        return render_template("index.html", logedin='True')
+
+#Sign up page
 @app.route("/signup")
 def signup():
     return render_template("signup.html", unsuccessful='False')
 
+#Page for successful registration
 @app.route("/registered", methods=["POST"])
 def registered():
     un = request.form.get("username")
@@ -52,38 +61,123 @@ def registered():
         db.execute("COMMIT")
         return render_template("registered.html")
 
+#Log in page
 @app.route("/login")
 def login():
     return render_template("login.html", unsuccessful=False)
 
-@app.route("/logedin", methods=["POST"])
+#Page for successful registration
+@app.route("/loggedin", methods=["POST"])
 def logedin():
     un = request.form.get("username")
     pw = request.form.get("password")
 
-    db.begin()
+    db.execute("BEGIN")
 
     storedPW = db.execute("SELECT password FROM accounts WHERE username = :username", {"username": un}).fetchone()
+    userID = db.execute("SELECT user_id FROM accounts WHERE username = :username", {"username": un}).fetchone()
 
-    db.commit()
+    db.execute("COMMIT")
 
     if(verify_password(storedPW[0], pw)):
         # TODO: set some sort of "loged in" variable to True
-        return render_template("logedin.html")
+        session["currentUserID"] = userID
+        return render_template("loggedin.html")
     else:
         return render_template("login.html", unsuccessful=True)
 
+#Logs user out of website
+@app.route("/logout")
+def logout():
+    session["currentUserID"]=None
+    return render_template("loggedout.html", logedin='False')
+
+#Page for catalog of books
+@app.route("/catalog")
+def catalog():
+
+    books = db.execute("SELECT * FROM books").fetchall();
+
+    return render_template("catalog.html", books=books)
+
+#Page showing search results in catalog
+@app.route("/search", methods=["POST"])
+def search():
+
+    keyword = request.form.get("search")
+    keyword = keyword.capitalize()
+
+    results = db.execute("SELECT * FROM books WHERE title LIKE :keyword", {"keyword": "%" + keyword + "%"}).fetchall()
+
+    return render_template("catalog.html", books=results)
+
+#Page showing details about book
+@app.route("/catalog/book/<string:isbn>")
+def book(isbn):
+
+    book = db.execute("SELECT * FROM books WHERE isbn=:isbn", {"isbn": isbn}).fetchone()
+    reviews = db.execute("SELECT * FROM reviews WHERE isbn=:isbn", {"isbn": isbn}).fetchall()
+
+    user = db.execute("SELECT username FROM accounts WHERE user_id=:id", {"id": session["currentUserID"][0]}).fetchone()
+    userReview = db.execute("SELECT * FROM reviews WHERE poster=:user", {"user": user[0]}).fetchall()
+
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "9fK9b9a8syf0Al6ZSywKaA", "isbns": isbn})
+    if res.status_code != 200:
+        raise Exception("ERROR: API request unsuccessful.")
+    data = res.json()
+    bookInfo = data["books"][0]
+
+    if userReview is None:
+        return render_template("catalogBook.html", book=book, reviews=reviews, info=bookInfo, canPost='True')
+    else:
+        return render_template("catalogBook.html", book=book, reviews=reviews, info=bookInfo, canPost='False')
+
+#Posting a review for a book
+@app.route("/catalog/book/<string:isbn>/post", methods=["POST"])
+def post(isbn):
+
+    review = request.form.get("review")
+
+    print(session["currentUserID"])
+    print(session["currentUserID"][0])
 
 
+    db.execute("BEGIN")
+
+    user = db.execute("SELECT username FROM accounts WHERE user_id=:id", {"id": session["currentUserID"][0]}).fetchone()
+
+    db.execute("INSERT INTO reviews (poster, review_text, isbn) VALUES (:poster, :review_text, :isbn)", {"poster": user[0], "review_text": review, "isbn": isbn})
+
+    db.execute("COMMIT")
+
+    return book(isbn)
+
+@app.route("/api/<string:isbn>")
+def api(isbn):
+
+    book = db.execute("SELECT * FROM books WHERE isbn=:isbn", {"isbn": isbn}).fetchone()
+    if book is None:
+        return jsonify({"ERROR: Invalid book ISBN."}), 404
+
+    count = db.execute("SELECT COUNT(*) FROM reviews WHERE isbn=:isbn", {"isbn": isbn}).fetchone()
+
+    return jsonify({
+        "title": book.title,
+        "author": book.author,
+        "year": book.year,
+        "isbn": isbn,
+        "review_count": count[0]
+    })
+
+
+
+
+#Function to hash passwords
 def hash_password(password):
 
     return sha256_crypt.encrypt(password)
 
-
-
-
-
-
+#Function to verify passwords
 def verify_password(stored_password, provided_password):
 
 
